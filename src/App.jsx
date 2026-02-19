bce814c84a773da782778125dc6ab4fcdae15fba
gitnayemurrashid <nrsrabon17@gmail.com>
2026-02-19 14:21:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductCard from './components/ProductCard'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <ProductCard src="src/assets/shoe.png"/>
+      <ProductCard src="src/assets/shoe1.png"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const ProductCard = ({src}) => {
+  return (
+    <div className="page">
+      <div className="card">
+        <div className="badge"></div>
+
+        <div className="image-box">
+          <img
+            src={src}
+          />
+        </div>
+
+        <div className="content">
+          <h1>Lorem Ipsum <br /> Dolor Sit</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
+          </p>
+
+          <div className="price-rating">
+            <h1>$20</h1>
+
+            <div className="rating">
+              <span>⭐⭐⭐⭐☆</span>
+              <p>Discount 50%</p>
+            </div>
+          </div>
+        </div>
+
+        <button className="button">ADD TO CART</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
